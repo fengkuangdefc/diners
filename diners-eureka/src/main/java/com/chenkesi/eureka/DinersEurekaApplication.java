02417bb328654fc75617ba979b5bf2409ae8f19c
MYM <mym@MacBook-Pro.local>
2021-03-04 22:54:16 +0800

    Initial commit

---
+++ diners-eureka/src/main/java/com/chenkesi/eureka/DinersEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.chenkesi.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinersEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinersEurekaApplication.class, args);
+	}
+
+}
---
+++ diners-eureka/src/test/java/com/chenkesi/eureka/DinersEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenkesi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinersEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
